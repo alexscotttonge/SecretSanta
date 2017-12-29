1aabe0f8bcb1348e7bd14caf32f306768c18fa31
Alex <alex@scotttonge.com>
2017-12-29 11:21:17 +0000

    corrected choose_pairs method to select pairs

--- lib/secret_santa.rb
+++ lib/secret_santa.rb
@@ -13,11 +13,14 @@ class SecretSanta
 
   def choose_pairs
     list.each do |secret_santa|
-      index = (rand * list.length).to_i
-      recipient = list[index]
-      unless recipient == secret_santa
-        list.delete_at(index)
-        santa_pairs << [secret_santa, recipient]
+      loop do
+        index = (rand * list.length).to_i
+        recipient = list[index]
+        unless recipient == secret_santa
+          list.delete_at(index)
+          santa_pairs << [secret_santa, recipient]
+          break
+        end
       end
     end
   end
